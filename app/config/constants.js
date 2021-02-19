e9d05d5b94749977034a5f40149b0f4f5b87542d
Matthijs Meijerink <matthijsmeijerink97@gmail.com>
2021-02-19 18:14:11 +0100

    Confix fix 3

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,4 +1,5 @@
 const APP_DATA_NAME = "Mystical Machines";
-const DISTRIBUTION_URL = "http://launcher.mysticalmachines.com/distribution.json";
+const LIVE_DISTRIBUTION_URL = 'http://launcher.mysticalmachines.com/distribution.json'
+const DEV_DISTRIBUTION_URL = 'http://launcher.mysticalmachines.com/distribution.json'
 
-module.exports = { APP_DATA_NAME, DISTRIBUTION_URL };
+module.exports = { APP_DATA_NAME, LIVE_DISTRIBUTION_URL, DEV_DISTRIBUTION_URL }
